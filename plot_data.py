a9457b3980d8098f8f688c25588324a83f7da0b8
Chuan Khuna <chuan.khuna@gmail.com>
2019-07-11 10:00:40 +0700

    Edit plot option

--- plot_data.py
+++ plot_data.py
@@ -16,39 +16,43 @@ df = FD(data_file, station)
 
 def annual_plot(data, index):
     fig, ax = plt.subplots()
-    ax.plot(data.index, index, data=data)
+    ax.plot(data.index, index, '.', data=data)
+    ax.plot(data.index, index, data=data, alpha=0.3)
     years = mdates.YearLocator()
     months = mdates.MonthLocator()
-    years_fmt = mdates.DateFormatter('%Y-%m')
+    years_fmt = mdates.DateFormatter('%Y')
     ax.xaxis.set_major_locator(years)
     ax.xaxis.set_major_formatter(years_fmt)
     ax.xaxis.set_minor_locator(months)
-    plt.xticks(rotation=90)
-    ax.grid(True)
+    fig.autofmt_xdate()
+    plt.title(f'Annual plot: {index}')
+    plt.grid(True, alpha=0.5)
     plt.show()
 
 def monthly_plot(data, index):
     fig, ax = plt.subplots()
-    ax.plot(data.index, index, data=data)
+    ax.plot(data.index, index, '.', data=data)
+    ax.plot(data.index, index, data=data, alpha=0.3)
     plt.xticks(rotation=45)
-    ax.grid(True)
+    plt.grid(True, alpha=0.5)
+    plt.title(f'Monthly plot: {index}')
     plt.show()
-
 
 def histplot(data, index):
     sns.distplot(data[index], kde=False)
+    plt.title(f'Histogramplot plot: {index}')
     plt.show()
 
 
 
-# index = 'PRCP'
-# annual_data = df.select_annual(index)
-# annual_plot(annual_data, index)
-# histplot(annual_data, index)
+index = 'PRCP'
+annual_data = df.select_annual(index)
+annual_plot(annual_data, index)
+histplot(annual_data, index)
 
 
 index = 'TMAX'
 month_data = df.select_month(index, 2001, 4)
 month_data[index] = (month_data[index]-32)*5/9
-# histplot(month_data, index)
+histplot(month_data, index)
 monthly_plot(month_data, index)
