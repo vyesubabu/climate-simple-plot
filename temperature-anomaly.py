30f53af99b35a960bb63c8756ccfe83a5d168081
Chuan Khuna <chuan.khuna@gmail.com>
2019-07-17 13:38:33 +0700

    Add Regression in graph

--- temperature-anomaly.py
+++ temperature-anomaly.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pygal
 from datetime import datetime
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('./data/1951-2019.csv')
 df['DATE'] = pd.to_datetime(df['DATE'])
@@ -22,6 +23,12 @@ tavg_anomaly = df1['TAVG'] - baseline['TAVG']
 print(f'anomaly head: \n{tavg_anomaly.head()}\n')
 
 plt.title('AVG Temperature Anomaly, baseline: 1961-1991')
-sns.lineplot(data=tavg_anomaly)
+sns.scatterplot(data=tavg_anomaly)
+sns.lineplot(data=tavg_anomaly, alpha=0.5)
 plt.grid(True, alpha=0.3)
+
+x = np.array(tavg_anomaly.index).reshape(-1, 1)
+y = np.array(tavg_anomaly)
+regression = LinearRegression().fit(x, y)
+plt.plot(x, x*regression.coef_+regression.intercept_)
 plt.show()
