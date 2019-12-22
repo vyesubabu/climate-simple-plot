9a1c4c57bf61ba16a428ec8ad17aa1db0eccedda
unknown <chuan.khuna@gmail.com>
2019-12-22 16:39:02 +0700

    Update mapplot

--- mapplot.py
+++ mapplot.py
@@ -19,16 +19,16 @@ def plot_map(
     dpi=200,
 ):
     fig = plt.figure(figsize=figsize, dpi=dpi)
-    m = Basemap(projection="cyl")
+    m = Basemap(projection="cyl", resolution='i', area_thresh=5000)
 
     if draw_graticule:
         parallels = np.arange(-90, 90 + parallel_s, parallel_s)
         meridians = np.arange(-180, 180 + meridian_s, meridian_s)
-        m.drawparallels(parallels, labels=[1, 0, 0, 0], color="#62757f", linewidth=0.5)
-        m.drawmeridians(meridians, labels=[1, 0, 0, 1], color="#62757f", linewidth=0.5)
+        m.drawparallels(parallels, labels=[1, 0, 0, 0], color="#62757f", linewidth=0.01)
+        m.drawmeridians(meridians, labels=[1, 0, 0, 1], color="#62757f", linewidth=0.01)
 
-    m.drawcountries()
-    m.drawcoastlines()
+    m.drawcountries(linewidth=0.1)
+    m.drawcoastlines(linewidth=0.1)
 
     x, y = m(*np.meshgrid(lons, lats))
     color = m.pcolor(
@@ -37,6 +37,6 @@ def plot_map(
     m.colorbar(color, location="bottom", pad="10%")
 
     if save_file:
-        plt.savefig(f"result_{int(time.time())}.png", dpi=dpi * 1.5)
+        plt.savefig(f"result_{int(time.time())}.png", dpi=dpi * 10)
 
     plt.show()
